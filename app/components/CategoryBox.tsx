8a4c13b442537a5af4d3b5e23e7cff8808f23c81
sherlyastha <asthapandeycse9@gmail.com>
2023-04-27 00:45:01 +0530

    adding categorybox below UserMenu

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -9,15 +9,31 @@ selected? :boolean
 
 
 const  CategoryBox:React.FC <CategoryBoxProps> = ({
-    icon,
+    icon: Icon,
     label,
     selected
 }) => {
-    return (<div >
-
-
-
-    </div>  );
+    return (<div
+            className={`
+            flex 
+            flex-col 
+            items-center 
+            justify-center 
+            gap-2
+            p-3
+            border-b-2
+            hover:text-neutral-800
+            transition
+            cursor-pointer
+            ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+            ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+        `}>
+            
+        <Icon size={26} />
+        <div className="font-medium text-sm">
+        {label}
+      </div>
+        </div>  );
 }
  
 export default  CategoryBox;
