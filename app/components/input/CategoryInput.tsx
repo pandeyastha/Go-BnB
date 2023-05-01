d23e900745eb47ec8c1757c40dc58fc4bff52ad0
sherlyastha <asthapandeycse9@gmail.com>
2023-05-01 14:20:25 +0530

    rentModal upd

---
+++ app/components/input/CategoryInput.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { IconType } from "react-icons";
+
+
+
+
+interface CategoryInputProps{
+icon :IconType;
+label:string;
+selected? :boolean;
+onClick: (value:string) =>void;
+}
+
+const CategoryInput: React.FC<CategoryInputProps> = (
+{icon: Icon,
+label,
+selected ,
+onClick
+}
+) => {
+    return (<div>
+        hi
+
+    </div>  );
+}
+ 
+export default CategoryInput;
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -13,6 +13,9 @@ import { useMemo, useState } from "react";
 
 import useRentModal from '@/app/hooks/useRentModal';
 import Modal from './Modal';
+import Heading from '../Heading';
+import { categories } from '../navbar/Categories';
+import CategoryInput from '../input/CategoryInput';
 
 
 enum STEPS {
@@ -55,6 +58,43 @@ const RentModal = () => {
     return 'Back'
   }, [step]);
      
+  let bodyContent =(
+    <div className="flex flex-col gap-8">
+      <Heading 
+      title= "Which of these best describes your place?"
+      subtitle='Pick a category'/>
+
+        <div className='
+        grid
+        grid-cols-1
+        md:grid-cols-2
+        gap-3
+        max-h-[50vh]
+        overflow-y-auto
+        '
+        >
+          {categories.map((item) => (
+            <div key= {item.label} className='col-span-1'>
+              <CategoryInput
+              onClick={ () => {}}
+              selected={false}
+              label={item.label}
+              icon={item.icon} 
+              />
+            </div>
+          ))}
+        </div>
+
+    </div>
+  )
+
+
+
+
+
+
+
+
 
   return (
     <Modal
@@ -65,7 +105,7 @@ const RentModal = () => {
     secondaryActionLabel={secondaryActionLabel}
     secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
     title="AirBnb Your Home!"
-
+    body={bodyContent}
     />
   );
 }
